3ce98f4efef6337021efdb63bea4a42bf00e5771
Paul Maddox <pmaddox@amazon.com>
2018-01-15 12:00:28 +0000

    Initial commit

---
+++ main_test.go
@@ -0,0 +1,41 @@
+package main_test
+
+import (
+	"testing"
+
+	main "github.com/aws-samples/lambda-go-samples"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHandler(t *testing.T) {
+
+	tests := []struct {
+		request events.APIGatewayProxyRequest
+		expect  string
+		err     error
+	}{
+		{
+			// Test that the handler responds with the correct response
+			// when a valid name is provided in the HTTP body
+			request: events.APIGatewayProxyRequest{Body: "Paul"},
+			expect:  "Hello Paul",
+			err:     nil,
+		},
+		{
+			// Test that the handler responds ErrNameNotProvided
+			// when no name is provided in the HTTP body
+			request: events.APIGatewayProxyRequest{Body: ""},
+			expect:  "",
+			err:     main.ErrNameNotProvided,
+		},
+	}
+
+	for _, test := range tests {
+		response, err := main.Handler(test.request)
+		assert.IsType(t, test.err, err)
+		assert.Equal(t, test.expect, response.Body)
+	}
+
+}
